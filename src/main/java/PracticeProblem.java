290384ddbc0df2eaaab2157c652be048a1ab5eef
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-02-20 16:00:40 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,30 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String pyramid(int rows) {
+		String drawing = "";
+		for (int i = rows - 1; i >= 0; --i) {
+			drawing += " ".repeat(i) + '*';
+			drawing += " *".repeat(rows - i - 1);
+			if (i != 0) drawing += '\n';
+		}
+		return drawing;
+	}
 
+	public static String square(int length) {
+		if (length < 0) return "";
+		String drawing = "*".repeat(length); // first row
+		if (length > 1) {
+			for (int i = 0; i < length - 2; ++i) drawing += "\n*" + " ".repeat(length - 2) + '*';
+			drawing += '\n' + "*".repeat(length);
+		}
+		return drawing;
+	}
+
+	public static boolean hasLowercase(String word) {
+		for (int i = 0; i < word.length(); ++i) {
+			if (Character.isLowerCase(word.charAt(i))) return true;
+		}
+		return false;
 	}
 
 }
--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
@@ -115,7 +116,7 @@ void squareTest2() {
 }
     catch(Exception e) {
         fail("Something weird happened");
-}
+    }
 }
 @Test
 @DisplayName("")
